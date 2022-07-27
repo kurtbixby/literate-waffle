f7372f4aa9d90d456476e351db83bc399a664d08
Kurt Bixby <kurtbixby@mac.com>
2022-07-26 20:09:11 -0500

    Fixed bug in Update Employee sp

--- db/stored_procedures/update_employee_role.sql
+++ db/stored_procedures/update_employee_role.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE sp_update_employee_role(employee_id INT, role_id INT)
 BEGIN
     UPDATE employee
     SET employee.role_id = role_id
-    VALUES employee.id = employee_id;
+    WHERE employee.id = employee_id;
 END //
 
 DELIMITER ;
