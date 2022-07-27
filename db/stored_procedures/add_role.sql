9eee072c773663bd132b5afa3136f717db81cc42
Kurt Bixby <kurtbixby@mac.com>
2022-07-26 20:28:58 -0500

    Fixed formatting and bugs in SPs

--- db/stored_procedures/add_role.sql
+++ db/stored_procedures/add_role.sql
@@ -1,9 +1,9 @@
 DELIMITER //
 
-CREATE PROCEDURE sp_add_role(name VARCHAR(30), salary INT, department_id INT)
+CREATE PROCEDURE sp_add_role(title VARCHAR(30), salary INT, department_id INT)
 BEGIN
-    INSERT INTO role (name, salary, department_id)
-    VALUES (name, salary, department_id);
+    INSERT INTO role (title, salary, department_id)
+    VALUES (title, salary, department_id);
 END //
 
 DELIMITER ;
--- db/stored_procedures/get_roles.sql
+++ db/stored_procedures/get_roles.sql
@@ -2,7 +2,7 @@ DELIMITER //
 
 CREATE PROCEDURE sp_get_roles()
 BEGIN
-    select r.id, r.title, r.salary, d.name from role r INNER JOIN department d on r.department_id = d.id;
+    SELECT r.id AS 'Id', r.title AS 'Title', r.salary AS 'Salary', d.name AS 'Department' from role r INNER JOIN department d on r.department_id = d.id;
 END //
 
 DELIMITER ;
