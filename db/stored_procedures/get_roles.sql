1c683826d39184f03fefd97aa3f0449efa05bfa6
Kurt Bixby <kurtbixby@mac.com>
2022-07-27 13:44:31 -0500

    Fixed Bug where roles weren't displayed if they were in a NULL department

--- db/stored_procedures/get_roles.sql
+++ db/stored_procedures/get_roles.sql
@@ -4,7 +4,7 @@ DELIMITER //
 
 CREATE PROCEDURE sp_get_roles()
 BEGIN
-    SELECT r.id AS 'Id', r.title AS 'Title', r.salary AS 'Salary', d.name AS 'Department' from role r INNER JOIN department d on r.department_id = d.id;
+    SELECT r.id AS 'Id', r.title AS 'Title', r.salary AS 'Salary', d.name AS 'Department' from role r LEFT JOIN department d on r.department_id = d.id;
 END //
 
 DELIMITER ;
